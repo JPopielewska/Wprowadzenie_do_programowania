d832a64a3074c22b30abe2e05f3a02e206f95891
JPopielewska <juliapopielewska@op.pl>
2022-07-23 12:05:24 +0200

    Dodanie skanera z imieniem, nazwiskiem i wiekiem

---
+++ src/TextFromUser.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TextFromUser {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj imie");
+        String imie = scanner.nextLine();
+        System.out.println("Podaj nazwisko");
+        String nazwisko = scanner.nextLine();
+        System.out.println("Podaj wiek");
+        int wiek = scanner.nextInt();
+        System.out.println("Hello" + imie + nazwisko + ". Masz " + wiek + " lat.");
+    }
+}
