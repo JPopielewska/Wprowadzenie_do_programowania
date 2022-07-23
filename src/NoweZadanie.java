ab5fd4370d369007cd394377e2a3e5d683e59bfc
JPopielewska <juliapopielewska@op.pl>
2022-07-23 14:05:45 +0200

    Darmowy szot

--- src/NoweZadanie.java
+++ src/NoweZadanie.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class NoweZadanie {
@@ -7,6 +8,7 @@ public class NoweZadanie {
          * 2. Chcę, żeby po zapytaniu o wiek była pętla sprawdzająca, czy osoba jest pełnoletnia, czy nie -
          * (przy pełnoletniej napis 'Zapraszam do klubu', przy niepełnoletniej  napis 'Wypad'
          */
+        String[] wygrani = new String[]{"Ania", "Zosia", "Magda", "Piotrek", "Adam"};
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj imię");
         String imie = scanner.next();
@@ -16,6 +18,11 @@ public class NoweZadanie {
         int wiek = scanner.nextInt();
         if (wiek >= 18) {
             System.out.println("Zapraszam do klubu");
+            for (String wygrany: wygrani){
+                if(Objects.equals(imie, wygrany)){
+                    System.out.println("Dostajesz darmowego szota;)");
+                }
+            }
         }
         if (wiek < 18) {
             System.out.println("Wypad");
